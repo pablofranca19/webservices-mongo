b1d011c89b94c128869bd486b29800d2c80229ca
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-03-04 08:54:33 -0300

    Create project

---
+++ src/main/java/com/pablofranca/webservicesmongo/WebservicesmongoApplication.java
@@ -0,0 +1,13 @@
+package com.pablofranca.webservicesmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablofranca/webservicesmongo/WebservicesmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablofranca.webservicesmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

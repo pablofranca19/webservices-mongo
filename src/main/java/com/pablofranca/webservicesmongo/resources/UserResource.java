112656e23b02093455c21d034814e9484f1f3c48
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-03-05 20:39:02 -0300

    Resources/controller layer

---
+++ src/main/java/com/pablofranca/webservicesmongo/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.pablofranca.webservicesmongo.resources;
+
+import com.pablofranca.webservicesmongo.domain.User;
+import com.pablofranca.webservicesmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll () {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+}
